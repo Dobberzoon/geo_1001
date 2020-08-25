8d3c7b23058861f2e5ef173b7d6a5473c834ce74
claragarciasan <C.Garcia-Sanchez@tudelft.nl>
2020-08-25 12:20:12 +0200

    changed folder names

---
+++ lectureA3/Lecture5-VariablesRelationship.py
@@ -0,0 +1,19 @@
+# Clara Garcia-Sanchez
+# 12/08/2020
+################################# DO NOT CHANGE #####################################
+# some_file.py
+import sys
+# insert at 1, 0 is the script path (or '' in REPL)
+sys.path.insert(1, '/Users/claragarciasan/Documents/TUD/Classes/GEO1001/geo1001.2020/common_functions/')
+import numpy as np																	#
+import matplotlib.pyplot as plt														#
+import pandas as pd																	#
+from analytic import ReadBabyBoom 
+import thinkstats2
+import thinkplot
+import brfss
+#####################################################################################
+df = brfss.ReadBrfss(nrows=None)
+sample = thinkstats2.SampleRows(df,5000)
+heights, wreights = sample.htm3, sample.wtkg2
+print(heights)
